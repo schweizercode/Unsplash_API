72f7cddd7106a619505edc1b1b7a4457f7823170
schweizercode <schweizercode@gmail.com>
2021-01-30 19:55:37 +0100

    commit

--- data.js
+++ data.js
@@ -281,7 +281,7 @@ const isDate = (fact) => {
         console.log('formatFactDate', formatFactDate)
         if (date === formatFactDate) {
 
-            return true
+            return false
         } else {
             return false
         }
@@ -312,6 +312,7 @@ const isInCheckboxMore = (fact) => {
     } else {
         return false
     }
+
 }
 
 
